cd3b68dc4f4c7cef1171cd990631e71398c8d5e1
Laura <dietz@cs.unh.edu>
2024-01-04 16:43:49 -0500

    Exam_score: (grading) with arpparse cmd arguments

--- exam_pp/exam_score.py
+++ exam_pp/exam_score.py
@@ -15,24 +15,51 @@ def fix_car_query_id(input:List[Tuple[str,List[QuestionPromptWithChoices]]]) -> 
 
 def main():
     """Score paragraphs by number of questions that are correctly answered."""
+
+    import argparse
+
+    desc = f'''EXAM grading, verifying which paragraphs answer which questions with a Q/A system. \n
+              The input and output file (i.e, exam_annotated_file) has to be a *JSONL.GZ file that follows this structure: \n
+              \n  
+                  [query_id, [FullParagraphData]] \n
+              \n
+               where `FullParagraphData` meets the following structure \n
+             {FullParagraphData.schema_json(indent=2)}
+             '''
+    
+    parser = argparse.ArgumentParser(description="EXAM grading"
+                                   , epilog=desc
+                                   , formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('paragraph_file', type=str, metavar='xxx.jsonl.gz'
+                        , help='json file with paragraph to grade with exam questions.The typical file pattern is `exam-xxx.jsonl.gz.'
+                        )
+
+    parser.add_argument('-o', '--out-file', type=str, metavar='exam-xxx.jsonl.gz', help='Output file name where paragraphs with exam grade annotations will be written to')
+    parser.add_argument('--max-queries', type=int, metavar='INT', default=None, help='limit the number of queries that will be processed (for debugging)')
+    parser.add_argument('--max-paragraphs', type=int, metavar='INT', default=None, help='limit the number of paragraphs that will be processed (for debugging)')
+
+    # Parse the arguments
+    args = parser.parse_args()  
+
     lesson_questions:Dict[str,List[QuestionPromptWithChoices]] = dict(fix_car_query_id(tqa_loader.load_all_tqa_data()))
     # print('question bank query ids', lesson_questions.keys())
     batchPipe = BatchingPipeline(BATCH_SIZE)
 
-    with gzip.open("output.jsonl.gz", 'wt', encoding='utf-8') as out_file:
+    with gzip.open(args.out_file, 'wt', encoding='utf-8') as out_file:
 
-        query_paragraphs:List[QueryWithFullParagraphList] = parseQueryWithFullParagraphs("./benchmarkY3test-qrels-with-text.jsonl.gz")
+        query_paragraphs = parseQueryWithFullParagraphs(args.paragraph_file)
         qa = McqaPipeline()
 
-        for queryWithFullParagraphList in query_paragraphs:
+        for queryWithFullParagraphList in itertools.islice(query_paragraphs, args.max_queries):
             query_id = queryWithFullParagraphList.queryId
             questions = lesson_questions.get(query_id)
             if questions is None:
+                
                 print(f'No exam question for query Id {query_id} available. skipping.')
                 continue
 
             paragraphs = queryWithFullParagraphList.paragraphs
-            for para in paragraphs:
+            for para in itertools.islice(paragraphs, args.max_paragraphs):
                 paragraph_id = para.paragraph_id
                 paragraph_txt = para.text
 
