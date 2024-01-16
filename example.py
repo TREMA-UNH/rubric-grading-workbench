0f4c296fb85db124fb46f09cf1b6170658ebc9e9
Laura <dietz@cs.unh.edu>
2024-01-15 21:51:01 -0500

    Adding Usage Example and README

--- example.py
+++ example.py
@@ -4,16 +4,16 @@ from exam_pp.exam_cover_metric import compute_exam_cover_scores, write_exam_resu
 from exam_pp.parse_qrels_runs_with_text import GradeFilter, QueryWithFullParagraphList, parseQueryWithFullParagraphs
 
 # Read Graded Exam files, compute EXAM Cover Evaluation Metric
-
-exam_factory = ExamCoverScorerFactory(GradeFilter.noFilter(), min_self_rating=None)
-
 query_paragraphs:List[QueryWithFullParagraphList] = parseQueryWithFullParagraphs(Path("exam-graded.jsonl.gz"))
-
+exam_factory = ExamCoverScorerFactory(GradeFilter.noFilter(), min_self_rating=None)
 resultsPerMethod = compute_exam_cover_scores(query_paragraphs, exam_factory=exam_factory, rank_cut_off=20)
 
-print(f'plain EXAM: {resultsPerMethod["my_method"].examScore}')
-print(f'normalized EXAM:  {resultsPerMethod["my_method"].nExamScore}')
+
+# Print Exam Cover Evaluation Scores
+for examEval in resultsPerMethod.values():
+    print(f'{examEval.method}  exam={examEval.examScore:0.2f}+/-{examEval.examScoreStd:0.2f} \t  n-exam={examEval.nExamScore:0.2f}')
 
 examEvaluationPerQuery:Dict[str,float] = resultsPerMethod['my_method'].examCoverPerQuery
 
+# Export Exam Cover Evaluation Scores
 write_exam_results("exam-eval.jsonl.gz", resultsPerMethod)
