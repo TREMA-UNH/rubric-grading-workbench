b63c58230f015b0ea705faee96f4d27df7c809ad
Laura <dietz@cs.unh.edu>
2024-01-19 13:36:18 -0500

    argparse: trying to avoid assertion errors
    workaround: call `python -O ...`

--- exam_pp/exam_post_pipeline.py
+++ exam_pp/exam_post_pipeline.py
@@ -458,6 +458,7 @@ def main(cmdargs=None):
 
     # Parse the arguments
     args = parser.parse_args(args=cmdargs)    
+        
     grade_filter = GradeFilter(model_name=args.model, prompt_class = args.prompt_class, is_self_rated=None, min_self_rating=None, question_set=args.question_set)
 
 
